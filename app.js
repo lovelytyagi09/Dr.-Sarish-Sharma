aa7814b5951a876e6f1b5fd1582a5cf6d355be0f
Lovely Tyagi <lovelytyagi180@example.com>
2026-04-07 13:58:34 +0530

    last commit

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Set EJS as templating engine
 app.set('view engine', 'ejs');
